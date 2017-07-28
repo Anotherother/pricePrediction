40799f4b3f8326f293b4fd1684e2b7f10c1a7e91
Jacob <jacob.filin@gmail.com>
2017-07-28 12:07:56 +0700

    1 day. 1 feature(close) + EWMA optimisation smoothing

---
+++ DevelopingModels/src/get_data.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+import pandas as pd
+
+# Набор функций для работы с БД
+
+def connect(user, password, db, host: str, port: int, echo=False):
+    url = 'postgresql+psycopg2://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+    eng = sqlalchemy.create_engine(url, client_encoding='utf8', echo=echo)
+    meta = sqlalchemy.MetaData(bind=eng)
+
+    return eng, meta
+
+def get_data_frame(pair: str = 'USDT_BTC', exchange: str = 'poloniex') -> pd.DataFrame:
+
+    engine, meta = connect(user='postgres', password='password', db='btc_dev', host='94.230.125.199', port=16432)
+    df = pd.read_sql_query(
+        "SELECT date, time, open, close, low, high, volume, pair.\"name\""
+        "FROM candlestick, pair, exchange WHERE candlestick.exchange_id = exchange.id "
+        "AND pair.id = candlestick.pair_id AND exchange.name = '" + exchange + "' AND candlestick.pair_id IN (SELECT pair.id FROM pair "
+        "WHERE pair.alias_id = (SELECT pair.id FROM pair WHERE pair.name = '" + pair + "') OR pair.name = '" + pair + "');",con=engine)
+    return df
