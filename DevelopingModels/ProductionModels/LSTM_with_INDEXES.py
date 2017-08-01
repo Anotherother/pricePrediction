20d890cef5f1f7c536e896b2cb2da45eca76ce72
Jacob <jacob.filin@gmail.com>
2017-08-01 15:55:24 +0700

    LSTM_name. Adapt_autoPasing

--- DevelopingModels/ProductionModels/LSTM_with_INDEXES.py
+++ DevelopingModels/ProductionModels/LSTM_with_INDEXES.py
@@ -60,16 +60,17 @@ def nextDayPrediction(typeBlockchain, stock):
     
     DJA = IdexDataframe("DJA")
     GSPC = IdexDataframe("GSPC")
-    IXK = IdexDataframe("IXK")
+    NYA = IdexDataframe("NYA")
 
     loaded = get_data.get_data_frame(typeBlockchain, stock)
     loaded.index = loaded.date
 
     loaded = loaded[['open', 'close', 'low', 'high', 'volume']]
-    df = pd.concat([DJA,GSPC, IXK, loaded], axis = 1, ignore_index=False)
+    df = pd.concat([DJA,GSPC, NYA, loaded], axis = 1, ignore_index=False)
     df = df.fillna(method='ffill')
-    df = df.dropna(axis = 1, thresh=10)
-    df = df.dropna(axis = 0, thresh=10)
+    df = df.dropna(axis = 0, how = 'any')
+
+
 
     x_scaler = MinMaxScaler()
     y_scaler = MinMaxScaler()
@@ -77,37 +78,44 @@ def nextDayPrediction(typeBlockchain, stock):
     all_df = df.copy()
 
     feature = ['OpenDJA', 'HighDJA', 'LowDJA', 'CloseDJA', 'Adj CloseDJA', 'VolumeDJA',
-       'OpenGSPC', 'HighGSPC', 'LowGSPC', 'CloseGSPC', 'Adj CloseGSPC',
-       'VolumeGSPC', 'open', 'low', 'high', 'volume']
+           'OpenGSPC', 'HighGSPC', 'LowGSPC', 'CloseGSPC', 'Adj CloseGSPC',
+           'VolumeGSPC', 'OpenNYA', 'HighNYA', 'LowNYA', 'CloseNYA',
+           'Adj CloseNYA', 'VolumeNYA', 'open',  'low', 'high', 'volume']
 
     x = all_df[feature].copy()
     y = all_df['close'].copy()
 
-    x = pd.ewma(x,2)
-    y = pd.ewma(y,2)
-    
+    #x = pd.ewma(x,2)
+    #y = pd.ewma(y,2)
+
     x[feature] = x_scaler.fit_transform(x)
 
     y = y_scaler.fit_transform(y.values.reshape(-1, 1))
     x['close'] = y
-    
+
     num_features = x.shape[1]
-    X_train, y_train = load.load_data(x, WINDOW, TrainTest = False)
-    
+    #X_train, y_train = load.load_data(x, WINDOW, TrainTest = False)
+    X_train, y_train, X_test, y_test = load.load_data(x, WINDOW, train_size= 1.0, TrainTest = True)
+
     model = build_model(input_shape=(WINDOW, num_features))
-    
+
     print('START FIT MODEL...')
-    
+
     start = time.time()
-    
-    model.fit(X_train, y_train, batch_size=32, epochs=500, verbose=1)
+
+    #history = History()
+    #history= model.fit(X_train, y_train, validation_data=(X_test, y_test),  batch_size=32, epochs=500,verbose=0,
+    #          callbacks=[history])
+
+    model.fit(X_train, y_train, batch_size=32, epochs=5, verbose=1)
     end = time.time()
 
     print ('Learning time: ', end-start)
-    
+
     today = time.strftime("_%d_%m_%Y")
-    
+
     pathModel = "../../models/model_5f_" + typeBlockchain + today +".h5"
+
     save_model(model, pathModel)
     
     del model
--- DevelopingModels/ProductionModels/src/get_data.py
+++ DevelopingModels/ProductionModels/src/get_data.py
@@ -19,3 +19,15 @@ def get_data_frame(pair: str = 'USDT_BTC', exchange: str = 'poloniex') -> pd.Dat
         "AND pair.id = candlestick.pair_id AND exchange.name = '" + exchange + "' AND candlestick.pair_id IN (SELECT pair.id FROM pair "
         "WHERE pair.alias_id = (SELECT pair.id FROM pair WHERE pair.name = '" + pair + "') OR pair.name = '" + pair + "');",con=engine)
     return df
+
+def get_specific_data_frame(name, owner) -> pd.DataFrame:
+    
+        engine, meta = connect(user='postgres', password='password', db='btc_dev', host='94.230.125.199', port=16432)
+        df = pd.read_sql_query(
+            "SELECT open,close,low,high,volume,date,adj_close,exchange.name AS exchange,pair.name AS pair "
+            "FROM candlestick, date_stamp, exchange, pair "
+            "WHERE candlestick.date_stamp_id = date_stamp.id AND candlestick.exchange_id = exchange.id "
+            "AND candlestick.pair_id = pair.id AND exchange.name = \'" + owner + "\' AND pair.name = \'" + name + "\'"
+            " ORDER BY date_stamp.date",
+            con=engine)
+        return df
