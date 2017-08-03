68875f5b16942163a106441b0b168219e6534ce6
Jacob <jacob.filin@gmail.com>
2017-08-03 07:59:40 +0700

    ...

--- DevelopingModels/ProductionModels/src/get_data.py
+++ DevelopingModels/ProductionModels/src/get_data.py
@@ -10,24 +10,32 @@ def connect(user, password, db, host: str, port: int, echo=False):
     meta = sqlalchemy.MetaData(bind=eng)
     return eng, meta
 
-def get_data_frame(pair: str = 'USDT_BTC', exchange: str = 'poloniex') -> pd.DataFrame:
+#"SELECT date, time, open, close, low, high, volume, pair.\"name\""
+#"FROM candlestick, pair, exchange WHERE candlestick.exchange_id = exchange.id "
+#"AND pair.id = candlestick.pair_id AND exchange.name = '" + exchange + 
+#"' AND candlestick.pair_id IN (SELECT pair.id FROM pair "
+#"WHERE pair.alias_id = (SELECT pair.id FROM pair WHERE pair.name = '" + pair + "') OR pair.name = '" + pair + "');"
 
-    engine, meta = connect(user='postgres', password='password', db='btccandles', host='94.230.125.199', port=16432)
+def get_data_frame(pair: str = 'USDT_BTC', exchange: str = 'poloniex') -> pd.DataFrame:
+    engine, meta = connect(user='postgres', password='password', db='data_prod', host='94.230.125.199', port=16432)
     df = pd.read_sql_query(
-        "SELECT date, time, open, close, low, high, volume, pair.\"name\""
-        "FROM candlestick, pair, exchange WHERE candlestick.exchange_id = exchange.id "
-        "AND pair.id = candlestick.pair_id AND exchange.name = '" + exchange + "' AND candlestick.pair_id IN (SELECT pair.id FROM pair "
-        "WHERE pair.alias_id = (SELECT pair.id FROM pair WHERE pair.name = '" + pair + "') OR pair.name = '" + pair + "');",con=engine)
+        "SELECT open,close,low,high,volume,date,adj_close,exchange.name AS exchange,pair.name AS pair "
+            "FROM candlestick, exchange, pair "
+            "WHERE candlestick.exchange_id = exchange.id "
+            "AND candlestick.pair_id = pair.id AND exchange.name = '" + exchange + "' AND pair.name = '" + pair + 
+            "' ORDER BY candlestick.date, candlestick.time"
+        ,con=engine)
     return df
 
 def get_specific_data_frame(name, owner) -> pd.DataFrame:
     
-        engine, meta = connect(user='postgres', password='password', db='btc_dev', host='94.230.125.199', port=16432)
-        df = pd.read_sql_query(
-            "SELECT open,close,low,high,volume,date,adj_close,exchange.name AS exchange,pair.name AS pair "
-            "FROM candlestick, date_stamp, exchange, pair "
-            "WHERE candlestick.date_stamp_id = date_stamp.id AND candlestick.exchange_id = exchange.id "
-            "AND candlestick.pair_id = pair.id AND exchange.name = \'" + owner + "\' AND pair.name = \'" + name + "\'"
-            " ORDER BY date_stamp.date",
-            con=engine)
+        #engine, meta = connect(user='postgres', password='password', db='btc_dev', host='94.230.125.199', port=16432)
+        #df = pd.read_sql_query(
+        #   "SELECT open,close,low,high,volume,date,adj_close,exchange.name AS exchange,pair.name AS pair "
+        #    "FROM candlestick, date_stamp, exchange, pair "
+        #    "WHERE candlestick.date_stamp_id = date_stamp.id AND candlestick.exchange_id = exchange.id "
+        #    "AND candlestick.pair_id = pair.id AND exchange.name = \'" + owner + "\' AND pair.name = \'" + name + "\'"
+        #    " ORDER BY date_stamp.date",
+        #    con=engine)
+        df = get_data_frame(name,owner)
         return df
